ddf1d0d386e2d930409fa0adec2e0abeb2f12857
Suraj Wate <suraj.wate@gmail.com>
2023-10-11 17:25:43 +0100

    update password

--- flaskblog/models.py
+++ flaskblog/models.py
@@ -1,6 +1,7 @@
-from flaskblog import db, login_manager
+from flaskblog import db, login_manager, app
 from datetime import datetime
 from flask_login import UserMixin
+from itsdangerous import URLSafeTimedSerializer as Serializer
 
 
 # Get a user by its id
@@ -15,6 +16,22 @@ class User(db.Model, UserMixin):
     image_file = db.Column(db.String(20), nullable=False, default="default.jpg")
     password = db.Column(db.String(60), nullable=False)
     posts = db.relationship("Post", backref="author", lazy=True)
+
+    # Generate a token for password reset
+    def get_reset_token(self, expires_sec=1800):
+        s = Serializer(app.config["SECRET_KEY"], expires_sec)
+        return s.dumps({"user_id": self.id}).decode("utf-8")
+    
+    # Verify the token for password reset
+    @staticmethod
+    def verify_reset_token(token):
+        s = Serializer(app.config["SECRET_KEY"])
+        try:
+            # Load the token
+            user_id = s.loads(token)["user_id"]
+        except:
+            return None
+        return User.query.get(user_id)
     
     def __repr__(self):
         return f"User('{self.username}', '{self.email}', '{self.image_file}')"
