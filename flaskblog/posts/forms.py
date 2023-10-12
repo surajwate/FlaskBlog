26c7da3232ee3c262b93892e93187b708c0f5577
Suraj Wate <suraj.wate@gmail.com>
2023-10-12 11:57:13 +0100

    forms setup

---
+++ flaskblog/posts/forms.py
@@ -0,0 +1,9 @@
+
+
+
+
+class PostForm(FlaskForm):
+    title = StringField("Title", validators=[DataRequired()])
+    content = TextAreaField("Content", validators=[DataRequired()])
+    submit = SubmitField("Post")
+
