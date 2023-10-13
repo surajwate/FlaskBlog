a99ae4c44eac780219cf845d2ea3cc9270eb3aa7
Suraj Wate <suraj.wate@gmail.com>
2023-10-13 17:43:11 +0100

    run app

--- flaskblog/users/utils.py
+++ flaskblog/users/utils.py
@@ -1,9 +1,9 @@
 import os
 import secrets
 from PIL import Image
-from flask import url_for
+from flask import url_for, current_app
 from flask_mail import Message
-from flaskblog import mail, app
+from flaskblog import mail
 
 def save_picture(form_picture):
     random_hex = secrets.token_hex(8)
@@ -12,7 +12,7 @@ def save_picture(form_picture):
     _, f_ext = os.path.splitext(form_picture.filename)
     picture_fn = random_hex + f_ext
     # Save the picture to the static folder
-    picture_path = os.path.join(app.root_path, "static/profile_pics", picture_fn)
+    picture_path = os.path.join(current_app.root_path, "static/profile_pics", picture_fn)
 
     # Resize the image before saving it
     output_size = (125, 125)
